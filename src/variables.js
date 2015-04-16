a5b032cff1707bdaf120f7b6a8ec88ccfae16f40
hengs <hengs@onid.oregonstate.edu>
2015-04-16 14:32:20 -0700

    Finishing Variables and strings

--- src/variables.js
+++ src/variables.js
@@ -10,15 +10,12 @@ in input `a`
 a: the original a number
 */
 function variableModification(a) {
-  var plus5;
-  var asString;
-  var yourNumberIs;
-  //your code here
-
-  //end your code
+      var plus5 = 5 + a;
+      var asString = a.toString();
+      var yourNumberIs = 'your number is ' + a + '.';
+  
   return [plus5, asString, yourNumberIs, a];
 }
-
 /*
 Input:
 b: could be anything
@@ -28,9 +25,12 @@ return true if b is a primitive string value (also known as a string literal),
 false otherwise
 */
 function isString(b) {
-  //your code here
-  return undefined;
-  //end your code
+  if(typeof b =='string' || b instanceof String)
+    return true;
+  
+  else
+   return false;
+  
 }
 
 /*
@@ -41,7 +41,10 @@ Output:
 return true if c is null, false otherwise
 */
 function isNull(c) {
-  //your code here
-  return undefined;
-  //end your code
+  if(c==null)
+    return true;
+  
+  else
+    return false;
+ 
 }
